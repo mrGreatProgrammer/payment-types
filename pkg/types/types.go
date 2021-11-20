dfd6aac088e79e84ceee0a0372e75aec82b101e8
Student <fhgf9048@gmail.com>
2021-11-20 11:44:01 +0500

    добавление статусов

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,11 +1,25 @@
 package types
 
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д.)
 type Money int64
 
+// Status представляет собой статус платежа
 type Category string
 
+//Status представляет собой статус платежа
+type Status string
+
+// Предопределённые статусы платежей
+  const (
+    StatusOk Status="OK"
+    StatusFail Status="FAIL"
+    StatusInProgress Status="INPROGRESS"
+  )
+
+//   Payment представляет информавцию о платеже
 type Payment struct {
  ID int
  Amount Money
  Category Category
+ Status Status
 }
